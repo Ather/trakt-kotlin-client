67903743b7be4c4054566b9f3f5ecf6c725907f6
Michael Haas <TripleFrequency@TripleFrequency.com>
2018-03-23 00:04:22 -0400

    Further improvements to migration to Instants and Gson serialization

--- src/main/java/media/thehoard/thirdparty/api/trakt/utils/Json.kt
+++ src/main/java/media/thehoard/thirdparty/api/trakt/utils/Json.kt
@@ -6,27 +6,11 @@ import com.google.gson.GsonBuilder
 import media.thehoard.thirdparty.api.trakt.utils.gson.DateSerializer
 import media.thehoard.thirdparty.api.trakt.utils.gson.InstantTypeAdapter
 import java.time.Instant
-
-import java.util.Date
+import java.util.*
 
 object Json {
-    private var gson: Gson? = null
-
-    fun getGson(): Gson {
-        if (gson == null)
-            gson = GsonBuilder()
-                    .registerTypeAdapter<Date>(typeAdapter = DateSerializer())
-                    .registerTypeAdapter<Instant>(typeAdapter = InstantTypeAdapter)
-                    .create()
-
-        return this.gson!!
-    }
-
-    fun <T> serialize(obj: T): String {
-        return getGson().toJson(obj)
-    }
-
-    fun <T> deserialize(json: String, outClass: Class<T>): T {
-        return getGson().fromJson(json, outClass)
-    }
+    val gson: Gson = GsonBuilder()
+            .registerTypeAdapter<Date>(typeAdapter = DateSerializer)
+            .registerTypeAdapter<Instant>(typeAdapter = InstantTypeAdapter)
+            .create()
 }
