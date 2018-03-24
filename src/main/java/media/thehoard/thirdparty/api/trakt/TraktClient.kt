76d8ff8cd5a3aa277a2d6ad7aa88a175c844b09c
Michael Haas <TripleFrequency@TripleFrequency.com>
2018-03-23 21:31:55 -0400

    Add TraktClient class

---
+++ src/main/java/media/thehoard/thirdparty/api/trakt/TraktClient.kt
@@ -0,0 +1,91 @@
+package media.thehoard.thirdparty.api.trakt
+
+import media.thehoard.thirdparty.api.trakt.authentication.TraktAuthentication
+import media.thehoard.thirdparty.api.trakt.authentication.TraktDeviceAuth
+import media.thehoard.thirdparty.api.trakt.authentication.TraktOAuth
+import media.thehoard.thirdparty.api.trakt.core.TraktConfiguration
+import media.thehoard.thirdparty.api.trakt.modules.*
+import media.thehoard.thirdparty.api.trakt.utils.TraktUtils
+
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class TraktClient internal constructor() {
+    val configuration: TraktConfiguration = TraktConfiguration()
+
+    val authentication: TraktAuthentication = TraktAuthentication(this)
+
+    val oAuth: TraktOAuth = TraktOAuth(this)
+
+    val deviceAuth: TraktDeviceAuth = TraktDeviceAuth(this)
+
+    val calendars: TraktCalendarsModule = TraktCalendarsModule(this)
+
+    val checkin: TraktCheckinModule = TraktCheckinModule(this)
+
+    val certifications: TraktCertificationsModule = TraktCertificationsModule(this)
+
+    val comments: TraktCommentsModule = TraktCommentsModule(this)
+
+    val genres: TraktGenresModule = TraktGenresModule(this)
+
+    val movies: TraktMoviesModule = TraktMoviesModule(this)
+
+    val networks: TraktNetworksModule = TraktNetworksModule(this)
+
+    val people: TraktPeopleModule = TraktPeopleModule(this)
+
+    val recommendations: TraktRecommendationsModule = TraktRecommendationsModule(this)
+
+    val scrobble: TraktScrobbleModule = TraktScrobbleModule(this)
+
+    val search: TraktSearchModule = TraktSearchModule(this)
+
+    val shows: TraktShowsModule = TraktShowsModule(this)
+
+    val seasons: TraktSeasonsModule = TraktSeasonsModule(this)
+
+    val episodes: TraktEpisodesModule = TraktEpisodesModule(this)
+
+    val sync: TraktSyncModule = TraktSyncModule(this)
+
+    val users: TraktUsersModule = TraktUsersModule(this)
+
+    val clientId: String
+        get() = authentication.clientId
+
+    val clientSecret: String
+        get() = authentication.clientSecret
+
+    val isValidForUseWithoutAuthorization: Boolean?
+        get() = TraktUtils.isNullOrEmpty(clientId) && !clientId.contains(" ")
+
+    val isValidForUseWithAuthorization: Boolean?
+        get() = isValidForUseWithoutAuthorization!! && authentication.isAuthorized
+
+    val isValidForAuthenticationProcess: Boolean?
+        get() = isValidForUseWithoutAuthorization!! && !TraktUtils.isNullOrEmpty(clientSecret) && !clientSecret
+                .contains(" ")
+
+    constructor(clientId: String) : this() {
+        setClientId(clientId)
+    }
+
+    constructor(clientId: String, clientSecret: String) : this(clientId) {
+        setClientSecret(clientSecret)
+    }
+
+    fun setClientId(traktClientId: String): TraktClient {
+        authentication.clientId = traktClientId
+        return this
+    }
+
+    fun setClientSecret(traktClientSecret: String): TraktClient {
+        authentication.clientSecret = traktClientSecret
+        return this
+    }
+
+    companion object {
+        val MAIN_THREAD_POOL = Executors.newCachedThreadPool()
+    }
+}
