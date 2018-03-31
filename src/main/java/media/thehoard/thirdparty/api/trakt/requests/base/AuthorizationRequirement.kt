4c2fe63452ca573a77dcfd8e489391f7d5f86ce3
Michael Haas <TripleFrequency@TripleFrequency.com>
2018-03-30 23:15:50 -0400

    Add base request enums

---
+++ src/main/java/media/thehoard/thirdparty/api/trakt/requests/base/AuthorizationRequirement.kt
@@ -0,0 +1,7 @@
+package media.thehoard.thirdparty.api.trakt.requests.base
+
+enum class AuthorizationRequirement {
+    Required,
+    NotRequired,
+    Optional
+}
---
+++ src/main/java/media/thehoard/thirdparty/api/trakt/requests/base/RequestObjectType.kt
@@ -0,0 +1,12 @@
+package media.thehoard.thirdparty.api.trakt.requests.base
+
+enum class RequestObjectType {
+    Unspecified,
+    Movies,
+    Shows,
+    Seasons,
+    Episodes,
+    People,
+    Comments,
+    Lists
+}
