1028bb295499c91fc9958b16f76292ec2e049d6b
Michael Haas <TripleFrequency@TripleFrequency.com>
2018-03-31 20:24:12 -0400

    Add certification request objects

--- src/main/java/media/thehoard/thirdparty/api/trakt/requests/certifications/CertificationRequests.kt
+++ src/main/java/media/thehoard/thirdparty/api/trakt/requests/certifications/CertificationRequests.kt
@@ -1,2 +1,20 @@
 package media.thehoard.thirdparty.api.trakt.requests.certifications
 
+import media.thehoard.thirdparty.api.trakt.objects.basic.implementations.TraktCertificationImpl
+import media.thehoard.thirdparty.api.trakt.requests.base.AGetRequestHasResponse
+
+internal sealed class ACertificationsRequest : AGetRequestHasResponse<TraktCertificationImpl>() {
+    override val uriPathParameters: Map<String, Any>? = mapOf()
+
+    override fun validate() {}
+}
+
+internal class MovieCertificationsRequest : ACertificationsRequest() {
+    override val uriTemplate: String
+        get() = "certifications/movies"
+}
+
+internal class ShowCertificationsRequest : ACertificationsRequest() {
+    override val uriTemplate: String
+        get() = "certifications/shows"
+}
