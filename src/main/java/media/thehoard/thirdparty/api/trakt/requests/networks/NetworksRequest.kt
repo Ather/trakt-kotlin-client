8391fa31df99c4dc8696d726b3cba0fd14f1f9f8
Michael Haas <TripleFrequency@TripleFrequency.com>
2018-03-31 23:04:10 -0400

    Add network request objects

---
+++ src/main/java/media/thehoard/thirdparty/api/trakt/requests/networks/NetworksRequest.kt
@@ -0,0 +1,13 @@
+package media.thehoard.thirdparty.api.trakt.requests.networks
+
+import media.thehoard.thirdparty.api.trakt.objects.basic.implementations.TraktNetworkImpl
+import media.thehoard.thirdparty.api.trakt.requests.base.AGetRequestHasResponse
+
+internal class NetworksRequest : AGetRequestHasResponse<TraktNetworkImpl>() {
+    override val uriTemplate: String = "networks"
+
+    override val uriPathParameters: Map<String, Any>?
+        get() = mapOf()
+
+    override fun validate() {}
+}
