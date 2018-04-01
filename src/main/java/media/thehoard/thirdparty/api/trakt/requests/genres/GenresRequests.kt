d790a3fe2fde4f18ee6a522c59e0b8c3cd3f319b
Michael Haas <TripleFrequency@TripleFrequency.com>
2018-03-31 21:57:59 -0400

    Add genre request objects

---
+++ src/main/java/media/thehoard/thirdparty/api/trakt/requests/genres/GenresRequests.kt
@@ -0,0 +1,22 @@
+package media.thehoard.thirdparty.api.trakt.requests.genres
+
+import media.thehoard.thirdparty.api.trakt.objects.basic.implementations.TraktGenreImpl
+import media.thehoard.thirdparty.api.trakt.requests.base.AGetRequestHasResponse
+
+internal class GenresMovieRequest : AGetRequestHasResponse<TraktGenreImpl>() {
+    override val uriTemplate: String = "genres/movies"
+
+    override val uriPathParameters: Map<String, Any>?
+        get() = mapOf()
+
+    override fun validate() {}
+}
+
+internal class GenresShowRequest : AGetRequestHasResponse<TraktGenreImpl>() {
+    override val uriTemplate: String = "genres/shows"
+
+    override val uriPathParameters: Map<String, Any>?
+        get() = mapOf()
+
+    override fun validate() {}
+}
