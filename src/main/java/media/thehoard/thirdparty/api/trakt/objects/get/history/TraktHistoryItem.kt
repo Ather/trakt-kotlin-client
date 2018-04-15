a72098236007e1568efb097ad3ed23ae59de6dec
Michael Haas <TripleFrequency@TripleFrequency.com>
2018-04-14 22:00:56 -0400

    Update history objects to use interfaces

--- src/main/java/media/thehoard/thirdparty/api/trakt/objects/get/history/TraktHistoryItem.kt
+++ src/main/java/media/thehoard/thirdparty/api/trakt/objects/get/history/TraktHistoryItem.kt
@@ -2,10 +2,10 @@ package media.thehoard.thirdparty.api.trakt.objects.get.history
 
 import media.thehoard.thirdparty.api.trakt.enums.TraktHistoryActionType
 import media.thehoard.thirdparty.api.trakt.enums.TraktSyncItemType
-import media.thehoard.thirdparty.api.trakt.objects.get.episodes.implementations.TraktEpisodeImpl
-import media.thehoard.thirdparty.api.trakt.objects.get.movies.implementations.TraktMovieImpl
-import media.thehoard.thirdparty.api.trakt.objects.get.seasons.implementations.TraktSeasonImpl
-import media.thehoard.thirdparty.api.trakt.objects.get.shows.implementations.TraktShowImpl
+import media.thehoard.thirdparty.api.trakt.objects.get.episodes.TraktEpisode
+import media.thehoard.thirdparty.api.trakt.objects.get.movies.TraktMovie
+import media.thehoard.thirdparty.api.trakt.objects.get.seasons.TraktSeason
+import media.thehoard.thirdparty.api.trakt.objects.get.shows.TraktShow
 import java.time.ZonedDateTime
 
 interface TraktHistoryItem {
@@ -17,12 +17,12 @@ interface TraktHistoryItem {
 
     var type: TraktSyncItemType
 
-    var movie: TraktMovieImpl?
+    var movie: TraktMovie?
 
-    var show: TraktShowImpl?
+    var show: TraktShow?
 
-    var season: TraktSeasonImpl?
+    var season: TraktSeason?
 
-    var episode: TraktEpisodeImpl?
+    var episode: TraktEpisode?
 
 }
--- src/main/java/media/thehoard/thirdparty/api/trakt/objects/get/history/implementations/TraktHistoryItemImpl.kt
+++ src/main/java/media/thehoard/thirdparty/api/trakt/objects/get/history/implementations/TraktHistoryItemImpl.kt
@@ -3,11 +3,11 @@ package media.thehoard.thirdparty.api.trakt.objects.get.history.implementations
 import com.google.gson.annotations.SerializedName
 import media.thehoard.thirdparty.api.trakt.enums.TraktHistoryActionType
 import media.thehoard.thirdparty.api.trakt.enums.TraktSyncItemType
-import media.thehoard.thirdparty.api.trakt.objects.get.episodes.implementations.TraktEpisodeImpl
+import media.thehoard.thirdparty.api.trakt.objects.get.episodes.TraktEpisode
 import media.thehoard.thirdparty.api.trakt.objects.get.history.TraktHistoryItem
-import media.thehoard.thirdparty.api.trakt.objects.get.movies.implementations.TraktMovieImpl
-import media.thehoard.thirdparty.api.trakt.objects.get.seasons.implementations.TraktSeasonImpl
-import media.thehoard.thirdparty.api.trakt.objects.get.shows.implementations.TraktShowImpl
+import media.thehoard.thirdparty.api.trakt.objects.get.movies.TraktMovie
+import media.thehoard.thirdparty.api.trakt.objects.get.seasons.TraktSeason
+import media.thehoard.thirdparty.api.trakt.objects.get.shows.TraktShow
 import java.time.ZonedDateTime
 
 data class TraktHistoryItemImpl(
@@ -16,8 +16,8 @@ data class TraktHistoryItemImpl(
         override var watchedAt: ZonedDateTime? = null,
         override var action: TraktHistoryActionType = TraktHistoryActionType.UNSPECIFIED,
         override var type: TraktSyncItemType = TraktSyncItemType.UNSPECIFIED,
-        override var movie: TraktMovieImpl? = null,
-        override var show: TraktShowImpl? = null,
-        override var season: TraktSeasonImpl? = null,
-        override var episode: TraktEpisodeImpl? = null
+        override var movie: TraktMovie? = null,
+        override var show: TraktShow? = null,
+        override var season: TraktSeason? = null,
+        override var episode: TraktEpisode? = null
 ) : TraktHistoryItem
